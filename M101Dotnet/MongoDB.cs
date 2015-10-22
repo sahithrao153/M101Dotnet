fc27acd24bde1769cdd339a45be1edd4434641d8
sahithrao153 <sahith.andel@gmail.com>
2015-10-21 21:37:37 -0400

    deleting records
    mongo .net

---
+++ M101Dotnet/MongoDB.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace M101Dotnet
+{
+    public class MongoDB
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+                
+
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+        static async Task MainAsync(string[] args)
+        {
+            var urlString = "mongodb://localhost:27017";
+            var client = new MongoClient(urlString);
+            var db = client.GetDatabase("students");
+            var collection = db.GetCollection<BsonDocument>("grades");
+            var filter = new BsonDocument("type","homework");
+           // var count = 0;
+            var sort = Builders<BsonDocument>.Sort.Ascending("student_id").Ascending("score");
+            var result = await collection.Find(filter).Sort(sort).ToListAsync();
+           var previous_id=-1 ;
+           var student_id=-1;
+            int count = 0;
+            foreach (var doc in result)         
+            {
+                
+                student_id = (int)doc["student_id"];
+                    //Console.WriteLine(student_id);
+                if (student_id != previous_id)
+                {
+                    count++;
+                    previous_id = student_id;
+                    Console.WriteLine("removing :{0} ", doc);
+                   // await collection.DeleteOneAsync(doc);
+
+                    await collection.DeleteManyAsync(doc);
+                }
+
+
+
+              // process document
+            }
+            Console.WriteLine(count);
+            //Console.WriteLine(coll.FindAsync<"">);
+        }
+       
+    }
+}
---
+++ M101Dotnet/Student.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace M101Dotnet
+{
+    class Student
+    {
+        public ObjectId  Id  { get; set; }
+        public int student_id { get; set; }
+        public string type { get; set; }
+        public double  score { get; set; }
+
+    }
+}
